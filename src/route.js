bd3335f3b0c3a119fec8402d5119624b2846a04a
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-06-26 00:35:26 -0300

    Fim do estágio 4

--- src/route.js
+++ src/route.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const questionController = require('./controllers/QuestionController')
+const roomController = require('./controllers/RoomControllers')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index'))
-route.get('/room', (req, res) => res.render('room'))
-route.get('/createPass', (req, res) => res.render('createPass'))
+route.get('/', (req, res) => res.render('index', {page: 'enterRoom'}))
+route.get('/createPass', (req, res) => res.render('index', {page: 'createPass'}))
+route.get('/room/:room', (req, res) => res.render('room'))
 
 // Formato que o formulário deve passar a informação
-route.post('/room/:room/:question/:action', questionController.index)
+route.post('/question/:room/:question/:action', questionController.index)
+route.post('/createRoom', roomController.create)
 
 
 module.exports = route
