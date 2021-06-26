5a0e0ba302de4c9c68464609de205d36cc8694df
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-06-26 10:08:46 -0300

    Pequenas mudanças

--- src/controllers/RoomControllers.js
+++ src/controllers/RoomControllers.js
@@ -8,7 +8,7 @@ module.exports = {
 
         for (var i = 0; i < 6; i++) {
             i === 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()
-        }
+    }
 
         await db.run(`INSERT INTO rooms (
             id,
